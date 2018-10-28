6d701750c34b3ae9cd7591c702c3d8c58d41f3f8
Katia Aresti <karesti@redhat.com>
2018-10-28 20:18:20 +0100

    modify to work with config code, hotrod or spring

--- reader/reader-near-code/src/main/java/org/infinispan/near/NearAppConfig.java
+++ reader/reader-near-code/src/main/java/org/infinispan/near/NearAppConfig.java
@@ -1,9 +1,16 @@
 package org.infinispan.near;
 
+import org.infinispan.client.hotrod.configuration.NearCacheMode;
 import org.infinispan.reader.AppConfig;
+import org.infinispan.spring.starter.remote.InfinispanRemoteCacheCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class NearAppConfig extends AppConfig {
 
+   @Bean
+   public InfinispanRemoteCacheCustomizer customizer() {
+      return b -> b.nearCache().maxEntries(30).mode(NearCacheMode.INVALIDATED).validate();
+   }
 }
