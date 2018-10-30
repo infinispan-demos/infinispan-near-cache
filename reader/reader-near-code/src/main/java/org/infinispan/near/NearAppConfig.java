e465578c6817a7b55800488812507dab41830f06
Katia Aresti <karesti@redhat.com>
2018-10-30 11:36:20 +0100

    application.yaml instead of properties

--- reader/reader-near-code/src/main/java/org/infinispan/near/NearAppConfig.java
+++ reader/reader-near-code/src/main/java/org/infinispan/near/NearAppConfig.java
@@ -1,8 +1,13 @@
 package org.infinispan.near;
 
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.NearCacheMode;
+import org.infinispan.common.AppData;
+import org.infinispan.common.Contributor;
 import org.infinispan.reader.AppConfig;
 import org.infinispan.spring.starter.remote.InfinispanRemoteCacheCustomizer;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +15,13 @@ import org.springframework.context.annotation.Configuration;
 public class NearAppConfig extends AppConfig {
 
    @Bean
+   @Qualifier(AppData.I8N_CONTRIBUTORS_CACHE_NAME)
+   public RemoteCache<Integer, Contributor> contributorsCache(RemoteCacheManager cacheManager) {
+      return cacheManager.getCache(AppData.I8N_CONTRIBUTORS_CACHE_NAME);
+   }
+
+   @Bean
    public InfinispanRemoteCacheCustomizer customizer() {
-      return b -> b.nearCache().maxEntries(30).mode(NearCacheMode.INVALIDATED).validate();
+      return b -> b.nearCache().maxEntries(30).mode(NearCacheMode.INVALIDATED).cacheNamePattern("i8n-.*").validate();
    }
 }
